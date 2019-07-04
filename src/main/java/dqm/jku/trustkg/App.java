a08b94c46b2ae5990037f8479e968bbe6971c351
lisehr <lisa@ehrlinger.cc>
2019-07-04 08:37:36 +0200

    Init maven project

---
+++ src/main/java/dqm/jku/trustkg/App.java
@@ -0,0 +1,13 @@
+package dqm.jku.trustkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
