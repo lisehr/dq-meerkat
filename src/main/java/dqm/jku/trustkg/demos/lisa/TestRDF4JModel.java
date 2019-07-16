e7b6df655f75a766d142e9c8baf846d002591747
Lisa-Marie Huber <lm.huber@yahoo.de>
2019-07-16 14:32:14 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/main/java/dqm/jku/trustkg/demos/lisa/TestRDF4JModel.java
@@ -0,0 +1,82 @@
+package dqm.jku.trustkg.demos.lisa;
+
+import java.io.IOException;
+
+import dqm.jku.trustkg.connectors.ConnectorCSV;
+import dqm.jku.trustkg.connectors.DSInstanceConnector;
+import dqm.jku.trustkg.dsd.elements.Attribute;
+import dqm.jku.trustkg.dsd.elements.Concept;
+import dqm.jku.trustkg.dsd.elements.Datasource;
+import org.eclipse.rdf4j.model.util.ModelBuilder;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.RepositoryResult;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.Rio;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.model.Statement;
+
+import dqm.jku.trustkg.graphdb.*;
+
+public class TestRDF4JModel {
+	public static void main(String args[]) {
+		// Create Connection to CSV Connector
+		DSInstanceConnector conn = new ConnectorCSV(
+				"src/main/java/dqm/jku/trustkg/resources/Telematic Device Report - Device Voltage.csv", ",", "\n",
+				"Device Voltage", true);
+
+		// Create Schema from it
+		Datasource ds;
+		try {
+			ds = conn.loadSchema();
+
+			ModelBuilder builder = new ModelBuilder();
+			builder.setNamespace("ex", "http://example.com/");
+
+			for (Concept c : ds.getConcepts()) {
+				for (Attribute a : c.getAttributes()) {
+					builder.namedGraph("ex:testGraph").subject("ex:" + c.toString()).add(RDF.TYPE,
+							"ex:" + a.toString());
+				}
+
+			}
+			
+			System.out.println();
+
+			Model m = builder.build();
+
+			// the size of contexts indicates how many graphs are stored in this model
+			// System.out.println(m.contexts().size());
+
+			for (Resource context : m.contexts()) {
+				System.out.println("Graph " + context + " contains: ");
+				Rio.write(m.filter(null, null, null, context), System.out, RDFFormat.TURTLE);
+			}
+
+			EmbeddedGraphDB db = new dqm.jku.trustkg.graphdb.EmbeddedGraphDB("//home//lisa//graphdb_test");
+
+			// activate for first time creation
+			//db.createRepository("test");
+			Repository testRep = db.getRepository("test");
+			RepositoryConnection repConn = testRep.getConnection();
+			
+			repConn.add(m);
+
+			try (RepositoryResult<Statement> result = repConn.getStatements(null, null, null);) {
+				while (result.hasNext()) {
+					Statement st = result.next();
+					System.out.println("db contains: " + st);
+				}
+			}
+			finally {
+				db.close();
+			}
+
+		} catch (IOException e) {
+			System.err.println("Could not load Schema!");
+		}
+
+	}
+}
