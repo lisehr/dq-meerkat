6e0f7f1bd458faee332f3b758312c7bd8b3f7c02
Lisa <lisa@ehrlinger.cc>
2019-12-07 21:29:54 +0100

    add influx to kg
    update getting started and add influx db export to kg

--- src/main/java/dqm/jku/trustkg/demos/GettingStarted.java
+++ src/main/java/dqm/jku/trustkg/demos/GettingStarted.java
@@ -1,10 +1,11 @@
-package dqm.jku.trustkg.demos.lisa;
+package dqm.jku.trustkg.demos;
 
 import java.io.IOException;
 
 import dqm.jku.trustkg.connectors.ConnectorCSV;
 import dqm.jku.trustkg.dsd.DSDKnowledgeGraph;
 import dqm.jku.trustkg.dsd.elements.Datasource;
+import dqm.jku.trustkg.influxdb.InfluxDBConnection;
 import dqm.jku.trustkg.util.Constants;
 
 public class GettingStarted {
@@ -12,13 +13,15 @@ public class GettingStarted {
   private static final int RDP_SIZE = 1000;
 
   public static void main(String[] args) throws IOException, NoSuchMethodException {
-    // Create Connection to CSV Connector
+	  
+	/** PHASE 2: SYSTEM SETUP & RDP CREATION *****/
+	// Create Connection to CSV Connector
     ConnectorCSV conn = new ConnectorCSV(Constants.RESSOURCES + "csv/DataCoSupplyChainDataset.csv", ",", "\n", "SupplyChain");
 
     // Init KG by loading DSD elements
     Datasource ds = conn.loadSchema("https://faw.jku.at", "sc:");
 
-    // Create Knowledge Graph and add (possibly multiple data sources)
+    // Create Knowledge Graph and add (possibly multiple) data sources
     DSDKnowledgeGraph kg = new DSDKnowledgeGraph("supplychain");
     kg.addDatasourceAndConnector(ds, conn);
 
@@ -30,6 +33,12 @@ public class GettingStarted {
 
     // Export KG to .ttl file
     kg.exportKGToFile("supplychain");
+    
+    
+    /** PHASE 2: CONTINUOUS DQ MONITORING *****/
+    InfluxDBConnection influx = new InfluxDBConnection();
+    kg.addProfilesToInflux(influx);
+    
   }
 
 }
