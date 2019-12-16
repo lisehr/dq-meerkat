86c8ce85bc0ca814fba092da4d0a46745db34947
Lisa <lisa@ehrlinger.cc>
2019-12-16 14:36:09 +0100

    Correct string isBlank

--- src/main/java/dqm/jku/trustkg/quality/profilingmetrics/singlecolumn/datatypeinfo/Decimals.java
+++ src/main/java/dqm/jku/trustkg/quality/profilingmetrics/singlecolumn/datatypeinfo/Decimals.java
@@ -1,7 +1,10 @@
 package dqm.jku.trustkg.quality.profilingmetrics.singlecolumn.datatypeinfo;
 
+import static dqm.jku.trustkg.quality.profilingmetrics.MetricTitle.dec;
+
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.cyberborean.rdfbeans.annotations.RDFBean;
 import org.cyberborean.rdfbeans.annotations.RDFNamespaces;
 
@@ -11,80 +14,78 @@ import dqm.jku.trustkg.dsd.records.RecordList;
 import dqm.jku.trustkg.quality.DataProfile;
 import dqm.jku.trustkg.quality.profilingmetrics.ProfileMetric;
 
-import static dqm.jku.trustkg.quality.profilingmetrics.MetricTitle.*;
-
 @RDFNamespaces({ "foaf = http://xmlns.com/foaf/0.1/", })
 @RDFBean("foaf:Decimals")
 public class Decimals extends ProfileMetric {
-  public Decimals() {
-    
-  }
-  
-  public Decimals(DataProfile d) {
-    super(dec, d);
-  }
+	public Decimals() {
 
-  @Override
-  public void calculation(RecordList rs, Object oldVal) {
-    Attribute a = (Attribute) super.getRefElem();
-    this.setValueClass(Integer.class);
-    if (a.getDataType() == Object.class) return;
-    if (a.getDataType() == Integer.class || a.getDataType() == Long.class || a.getDataType() == String.class) {
-      this.setValue(0);
-      return;
-    }
-    
-    int decimals;    
-    if (oldVal == null) decimals = 0;
-    else decimals = (int) oldVal;
-    for (Record r : rs) {
-      Object field = r.getField(a);
-      decimals = getDecimals(decimals, (Number) field);
-    }
-    if (decimals == -1) this.setValue(null);
-    else this.setValue(decimals);
-  }
+	}
 
-  private int getDecimals(int decimals, Number field) {
-    String numStr = field.toString();
-    if (numStr.isBlank() || numStr.isEmpty()) return -1;
-    int pointPos = numStr.indexOf('.');
-    int dec = numStr.length() - pointPos - 1;
-    if (dec > decimals) return dec;
-    else return decimals;
-  }
+	public Decimals(DataProfile d) {
+		super(dec, d);
+	}
 
-  @Override
-  public void calculationNumeric(List<Number> list, Object oldVal) throws NoSuchMethodException {
-    Attribute a = (Attribute) super.getRefElem();
-    this.setValueClass(Integer.class);
-    if (a.getDataType() == Object.class) return;
-    if (list.isEmpty()) {
-      this.setValue(null);
-      return;
-    }
-    if (a.getDataType() == Integer.class || a.getDataType() == Long.class || a.getDataType() == String.class) {
-      this.setValue(0);
-      return;
-    }
-    
-    int decimals;    
-    if (oldVal == null) decimals = 0;
-    else decimals = (int) oldVal;
-    for (Number n : list) {
-      decimals = getDecimals(decimals, (Number) n);
-    }
-    this.setValue(decimals);
-  }
+	@Override
+	public void calculation(RecordList rs, Object oldVal) {
+		Attribute a = (Attribute) super.getRefElem();
+		this.setValueClass(Integer.class);
+		if (a.getDataType() == Object.class) return;
+		if (a.getDataType() == Integer.class || a.getDataType() == Long.class || a.getDataType() == String.class) {
+			this.setValue(0);
+			return;
+		}
 
-  @Override
-  public void update(RecordList rs) {
-    calculation(rs, super.getValue());
-  }
+		int decimals;    
+		if (oldVal == null) decimals = 0;
+		else decimals = (int) oldVal;
+		for (Record r : rs) {
+			Object field = r.getField(a);
+			decimals = getDecimals(decimals, (Number) field);
+		}
+		if (decimals == -1) this.setValue(null);
+		else this.setValue(decimals);
+	}
 
-  @Override
-  protected String getValueString() {
-    return super.getSimpleValueString();
-  }
+	private int getDecimals(int decimals, Number field) {
+		String numStr = field.toString();
+		if (StringUtils.isBlank(numStr) || numStr.isEmpty()) return -1;
+		int pointPos = numStr.indexOf('.');
+		int dec = numStr.length() - pointPos - 1;
+		if (dec > decimals) return dec;
+		else return decimals;
+	}
+
+	@Override
+	public void calculationNumeric(List<Number> list, Object oldVal) throws NoSuchMethodException {
+		Attribute a = (Attribute) super.getRefElem();
+		this.setValueClass(Integer.class);
+		if (a.getDataType() == Object.class) return;
+		if (list.isEmpty()) {
+			this.setValue(null);
+			return;
+		}
+		if (a.getDataType() == Integer.class || a.getDataType() == Long.class || a.getDataType() == String.class) {
+			this.setValue(0);
+			return;
+		}
+
+		int decimals;    
+		if (oldVal == null) decimals = 0;
+		else decimals = (int) oldVal;
+		for (Number n : list) {
+			decimals = getDecimals(decimals, (Number) n);
+		}
+		this.setValue(decimals);
+	}
+
+	@Override
+	public void update(RecordList rs) {
+		calculation(rs, super.getValue());
+	}
+
+	@Override
+	protected String getValueString() {
+		return super.getSimpleValueString();
+	}
 
 }
