1176a75638fe1ed7a6ee7bace61251ad359eae06
Lisa <lisa@ehrlinger.cc>
2019-12-16 14:25:30 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/dqm/jku/trustkg/demos/repeatability/GettingStarted.java
+++ src/main/java/dqm/jku/trustkg/demos/repeatability/GettingStarted.java
@@ -3,7 +3,6 @@ package dqm.jku.trustkg.demos.repeatability;
 import java.io.IOException;
 
 import dqm.jku.trustkg.connectors.ConnectorCSV;
-import dqm.jku.trustkg.connectors.ConnectorMySQL;
 import dqm.jku.trustkg.dsd.DSDKnowledgeGraph;
 import dqm.jku.trustkg.dsd.elements.Datasource;
 import dqm.jku.trustkg.influxdb.InfluxDBConnection;
@@ -11,36 +10,48 @@ import dqm.jku.trustkg.util.Constants;
 
 public class GettingStarted {
 
-  private static final int RDP_SIZE = 1000;
+	private static final int RDP_SIZE = 1000;
 
-  public static void main(String[] args) throws IOException, NoSuchMethodException {
-	  
-	/** PHASE 2: SYSTEM SETUP & RDP CREATION *****/
-	// Create Connection to CSV Connector
-    ConnectorCSV conn = new ConnectorCSV(Constants.RESSOURCES + "csv/DataCoSupplyChainDataset.csv", ",", "\n", "SupplyChain");
-//	  ConnectorMySQL conn = ConnectorMySQL.getInstance("jdbc:mysql://localhost:3366/", "DataCo_Supplychain", "dquser", "dataQ4T!_pw");	
-    
-    // Init KG by loading DSD elements
-    Datasource ds = conn.loadSchema("https://faw.jku.at", "sc:");
+	public static void main(String[] args) throws IOException, NoSuchMethodException {
 
-    // Create Knowledge Graph and add (possibly multiple) data sources
-    DSDKnowledgeGraph kg = new DSDKnowledgeGraph("supplychain");
-    kg.addDatasourceAndConnector(ds, conn);
+		/** PHASE 2: SYSTEM SETUP & RDP CREATION *****/
+		
+		// Create Connection to Supplychain dataset (once with CSV connector for repeatability; and once with MySQL connector as used for the demo)
+//		ConnectorMySQL conn1 = ConnectorMySQL.getInstance("jdbc:mysql://localhost:3366/", "DataCo_Supplychain", args[0], args[1]);
+		ConnectorCSV conn1 = new ConnectorCSV(Constants.RESSOURCES + "csv/DataCoSupplyChainDataset.csv", ",", "\n", "SupplyChain");
+		
+		// Create Connection to Tributech car engine data stream (once with CSV connector for repeatability; and once with Cassandra connector as used for the demo)
+//		ConnectorCassandra conn2 = new ConnectorCassandra("bursa.scch.at", "Tributech-datastream", args[2], args[3]);
+		ConnectorCSV conn2 = new ConnectorCSV(Constants.RESSOURCES + "csv/DataCoSupplyChainDataset.csv", ",", "\n", "CarEngineStream");
+		
+		// Optionally, create connection to Test data
+//		ConnectorCSV conn3 = new ConnectorCSV(Constants.RESSOURCES + "csv/Test.csv", ",", "\n", "Test data");
+		
+		// Init KG by loading DSD elements
+		Datasource dsSC = conn1.loadSchema("https://faw.jku.at", "sc:");
+		Datasource dsCE = conn2.loadSchema("https://faw.jku.at", "ce:");
+//		Datasource dsTD = conn3.loadSchema("https://faw.jku.at", "td:");
 
-    // Annotate reference data profile to KG
-    kg.addDataProfile(RDP_SIZE);
+		// Create Knowledge Graph and add (possibly multiple) data sources
+		DSDKnowledgeGraph kg = new DSDKnowledgeGraph("automotive");
+		kg.addDatasourceAndConnector(dsSC, conn1);
+		kg.addDatasourceAndConnector(dsCE, conn2);
+//		kg.addDatasourceAndConnector(ds, conn);
 
-    // Persist KG to Embedded GraphDB
-    // done automatically
+		// Annotate reference data profile to KG
+		kg.addDataProfile(RDP_SIZE);
 
-    // Export KG to .ttl file
-    kg.exportKGToFile("supplychain");
-    
-    
-    /** PHASE 2: CONTINUOUS DQ MONITORING *****/
-    InfluxDBConnection influx = new InfluxDBConnection();
-    kg.addProfilesToInflux(influx);
-    
-  }
+		// Persist KG to Embedded GraphDB
+		// done automatically
 
+		// Export KG to .ttl file
+		kg.exportKGToFile("supplychain");
+
+
+		/** PHASE 2: CONTINUOUS DQ MONITORING *****/
+		InfluxDBConnection influx = new InfluxDBConnection();
+		kg.addProfilesToInflux(influx);
+		
+		
+	}
 }
