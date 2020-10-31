91ab083401a9498711e2cef11217c8907cf8c1ff
Alexander Gindlhumer <alexander.gindlhumer@jku.at>
2020-10-31 10:53:20 +0100

    refactored demo packagenames in a more meaningful way
    more concrete information == less confusion

---
+++ src/main/java/dqm/jku/trustkg/demos/architecture/kg/GettingStartedAlternative.java
@@ -0,0 +1,48 @@
+package dqm.jku.trustkg.demos.architecture.kg;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dqm.jku.trustkg.connectors.DSConnector;
+import dqm.jku.trustkg.dsd.DSDKnowledgeGraph;
+import dqm.jku.trustkg.util.FileSelectionUtil;
+
+/**
+ * Alternative getting started class for first steps with DQ-MeeRKat
+ * 
+ * @author optimusseptim
+ *
+ */
+public class GettingStartedAlternative {
+  private static final String CSV_PATH = "src/main/java/dqm/jku/trustkg/resources/csv/";
+
+  public static void main(String[] args) throws IOException, NoSuchMethodException {
+    File directory = new File(CSV_PATH);
+    int fileCount = directory.list().length;
+
+    // Init KG by loading DSD elements
+    List<DSConnector> conns = new ArrayList<>();
+    for (int i = 0; i < fileCount; i++) conns.add(FileSelectionUtil.connectToCSV(i));
+
+    // Create Knowledge Graph and add (possibly multiple data sources)
+    DSDKnowledgeGraph kg = new DSDKnowledgeGraph("CSVTest");
+    kg.addDatasourcesViaConnectors(conns);
+
+    // Annotate reference data profile to KG
+    kg.addDataProfile();
+
+    // Persist KG to Embedded GraphDB
+    // done automatically
+
+    // Export KG to .ttl file
+    // kg.exportKGToFile("CSVTest");
+
+    // Export data profiles to CSV files
+    // kg.exportToCSV();
+
+    // Export KG-Report to textfile
+    kg.exportReport();
+  }
+}
---
+++ src/main/java/dqm/jku/trustkg/demos/architecture/rdf/TestRDFBeansDemo.java
@@ -0,0 +1,88 @@
+package dqm.jku.trustkg.demos.architecture.rdf;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.cyberborean.rdfbeans.RDFBeanManager;
+import org.cyberborean.rdfbeans.exceptions.RDFBeanException;
+import org.eclipse.rdf4j.common.iteration.CloseableIteration;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.RepositoryException;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.sail.nativerdf.NativeStore;
+
+import dqm.jku.trustkg.blockchain.standardchain.BlockChain;
+import dqm.jku.trustkg.connectors.DSConnector;
+import dqm.jku.trustkg.dsd.elements.Attribute;
+import dqm.jku.trustkg.dsd.elements.Concept;
+import dqm.jku.trustkg.dsd.elements.Datasource;
+import dqm.jku.trustkg.dsd.records.RecordList;
+import dqm.jku.trustkg.util.FileSelectionUtil;
+
+/**
+ * Test class for RDFBeans functionality
+ * 
+ * @author optimusseptim
+ *
+ */
+public class TestRDFBeansDemo {
+//  private static final boolean DEBUG = false;
+
+  @SuppressWarnings("deprecation")
+  public static void main(String args[]) throws Exception {
+    DSConnector conn = FileSelectionUtil.connectToCSV(1);
+
+    File dataDir = new File("./testrepo");
+    SailRepository repo = new SailRepository(new NativeStore(dataDir));
+    repo.initialize();
+
+    try (RepositoryConnection con = repo.getConnection()) {
+      RDFBeanManager manager = new RDFBeanManager(con);
+
+      Datasource ds;
+      ds = conn.loadSchema();
+      for (Concept c : ds.getConcepts()) {
+        System.out.println(c.getURI());
+        RecordList rs = conn.getRecordList(c);
+        for (Attribute a : c.getAttributes()) {
+          a.annotateProfile(rs);
+
+          System.out.println(a.getDataType().getSimpleName() + "\t" + a.getURI());
+          a.printAnnotatedProfile();
+        }
+        System.out.println();
+      }
+
+      manager.add(ds);
+
+      CloseableIteration<Datasource, Exception> iter = manager.getAll(Datasource.class);
+      while (iter.hasNext()) {
+        Datasource res = iter.next();
+        System.out.println(res.getURI() + " is equal to original Datasource? " + res.equals(ds));
+      }
+      iter.close();
+
+      // Testing blockchain aspect
+      BlockChain bc = new BlockChain(5, "test");
+      ds.fillBlockChain(bc);
+      System.out.println(bc.isChainValid());
+
+      manager.add(bc);
+
+      CloseableIteration<BlockChain, Exception> iterBC = manager.getAll(BlockChain.class);
+      while (iterBC.hasNext()) {
+        BlockChain res = iterBC.next();
+        System.out.println(res.isChainValid());
+        System.out.println("Restored BlockChain is equal to original Datasource? " + res.equals(bc));
+      }
+      iterBC.close();
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (RepositoryException e) {
+      e.printStackTrace();
+    } catch (RDFBeanException e) {
+      e.printStackTrace();
+    }
+  }
+}
