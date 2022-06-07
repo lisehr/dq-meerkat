1070669a311b6ec40eb33e730f3bf0837d0fcbea
Rainer Meindl <rainer.meindl@scch.at>
2022-06-07 08:42:44 +0200

    Add FullSkeletonGeneratorTests

--- src/test/java/dqm/jku/dqmeerkat/quality/generator/FullSkeletonGeneratorTest.java
+++ src/test/java/dqm/jku/dqmeerkat/quality/generator/FullSkeletonGeneratorTest.java
@@ -1,6 +1,7 @@
 package dqm.jku.dqmeerkat.quality.generator;
 
 import dqm.jku.dqmeerkat.dsd.elements.Attribute;
+import dqm.jku.dqmeerkat.dsd.elements.Concept;
 import dqm.jku.dqmeerkat.quality.DataProfile;
 import org.junit.Test;
 
@@ -8,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 
 /**
  * <h2>FullSkeletonGeneratorTest</h2>
- * <summary>TODO Insert do cheader</summary>
+ * <summary>Test class for {@link FullSkeletonGenerator}</summary>
  *
  * @author meindl, rainer.meindl@scch.at
  * @since 03.06.2022
@@ -28,5 +29,39 @@ public class FullSkeletonGeneratorTest {
         var ret = generator.generateSkeleton(profile);
         // then
         assertEquals(NR_OF_STATISTICS, ret.size());
+    }
+
+    @Test
+    public void testGenerateNull() {
+        // given
+        var attribute = new Attribute(); // no data type is set!
+        var generator = new FullSkeletonGenerator(attribute);
+        var profile = new DataProfile();
+        // when
+        var ret = generator.generateSkeleton(profile);
+        // then
+        assertEquals(0, ret.size());
+    }
+
+    @Test
+    public void testGenerateInvalid() {
+        var attribute = new Concept(); // concept is not correct type
+        var generator = new FullSkeletonGenerator(attribute);
+        var profile = new DataProfile();
+        // when
+        var ret = generator.generateSkeleton(profile);
+        // then
+        assertEquals(0, ret.size());
+    }
+
+    @Test
+    public void testGenerateElementNull() {
+        // given
+        var generator = new FullSkeletonGenerator(null);
+        var profile = new DataProfile();
+        // when
+        var ret = generator.generateSkeleton(profile);
+        // then
+        assertEquals(0, ret.size());
     }
 }
