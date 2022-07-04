7c9b4df964626ed1be53747d72380e15553fbc8d
Rainer Meindl <rainer.meindl@scch.at>
2022-07-04 11:17:25 +0200

    Fix MGSummary algorithm
    add if/else path when size of summary is >= k. Only add new items when
    size is smaller than k

--- src/main/java/dqm/jku/dqmeerkat/quality/profilingstatistics/singlecolumn/summary/MGSummaryProfileStatistic.java
+++ src/main/java/dqm/jku/dqmeerkat/quality/profilingstatistics/singlecolumn/summary/MGSummaryProfileStatistic.java
@@ -1,13 +1,14 @@
 package dqm.jku.dqmeerkat.quality.profilingstatistics.singlecolumn.summary;
 
 import dqm.jku.dqmeerkat.dsd.elements.Attribute;
-import dqm.jku.dqmeerkat.dsd.records.Record;
 import dqm.jku.dqmeerkat.dsd.records.RecordList;
 import dqm.jku.dqmeerkat.quality.DataProfile;
 import dqm.jku.dqmeerkat.quality.profilingstatistics.ProfileStatistic;
 import dqm.jku.dqmeerkat.quality.profilingstatistics.StatisticCategory;
 import dqm.jku.dqmeerkat.quality.profilingstatistics.StatisticTitle;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,35 +42,51 @@ public class MGSummaryProfileStatistic extends ProfileStatistic {
     @Override
     public void calculation(RecordList rs, Object oldVal) {
         var attribute = (Attribute) getRefElem();
-        var tmpMap = new HashMap<Object, Integer>();
-        for (Record record : rs) {
-            var value = record.getField(attribute.getLabel());
+        List<Number> list = rs.toList().stream().map(record -> (Number) record.getField(attribute.getLabel())).collect(Collectors.toList());
+        try {
+            calculationNumeric(list, null);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void calculationNumeric(List<Number> list, Object oldVal) throws NoSuchMethodException {
+        for (Number value : list) {
+            if (value instanceof Double) {
+                var symbols = DecimalFormatSymbols.getInstance();
+                symbols.setDecimalSeparator('.');
+                DecimalFormat df = new DecimalFormat("##.####", symbols);
+                value = Double.parseDouble(df.format(value));
+            }
+
+
             if (mkCounter.containsKey(value)) {
                 mkCounter.put(value, mkCounter.get(value) + 1);
             } else {
-                mkCounter.put(value, 1);
-            }
-            if (mkCounter.size() >= k) {
-                mkCounter = mkCounter.entrySet().stream()
-                        .map(objectIntegerEntry -> objectIntegerEntry.setValue(objectIntegerEntry.getValue() - 1))
-                        .collect(Collectors.toMap(o -> o, integer -> integer));
+                if (mkCounter.size() >= k) {
+                    var tmpMap = new HashMap<Object, Integer>();
+                    mkCounter = mkCounter.entrySet().stream()
+                            .peek(objectIntegerEntry -> objectIntegerEntry.setValue(objectIntegerEntry.getValue() - 1))
+                            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
-                mkCounter.forEach((o, integer) -> {
-                    if (integer > 0) {
-                        tmpMap.put(o, integer);
-                    }
-                });
-                mkCounter = tmpMap;
+                    mkCounter.forEach((o, integer) -> {
+                        if (integer > 0) {
+                            tmpMap.put(o, integer);
+                        }
+                    });
+                    mkCounter = tmpMap;
+                } else {
+                    mkCounter.put(value, 1);
+                }
+
             }
+
         }
         setValue(mkCounter);
         setValueClass(mkCounter.getClass());
     }
 
-    @Override
-    public void calculationNumeric(List<Number> list, Object oldVal) throws NoSuchMethodException {
-
-    }
 
     @Override
     public void update(RecordList rs) {
--- src/test/java/dqm/jku/dqmeerkat/quality/profilingstatistics/singlecolumn/summary/MGSummaryProfileStatisticTest.java
+++ src/test/java/dqm/jku/dqmeerkat/quality/profilingstatistics/singlecolumn/summary/MGSummaryProfileStatisticTest.java
@@ -1,29 +1,73 @@
 package dqm.jku.dqmeerkat.quality.profilingstatistics.singlecolumn.summary;
 
+import dqm.jku.dqmeerkat.dsd.elements.Attribute;
+import dqm.jku.dqmeerkat.dsd.records.RecordList;
+import dqm.jku.dqmeerkat.quality.DataProfile;
+import dqm.jku.dqmeerkat.util.FileSelectionUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 class MGSummaryProfileStatisticTest {
 
-    @Test
-    void calculationTest() {
-        // given
 
-        // when
+    private static Attribute dsdElement;
+    private static RecordList recordList;
 
-        // then
-        fail("Not implemented");
+    @BeforeAll
+    static void setup() throws IOException {
+        var conn = FileSelectionUtil.getConnectorCSV("src/test/resources/testRecordList.csv");
+        var ds = conn.loadSchema("http:/example.com", "hum");
+        var concept = ds.getConcepts().stream().findFirst().orElseThrow();
+        dsdElement = concept.getAttribute("values");
+        recordList = conn.getRecordList(concept);
     }
 
     @Test
-    void calculationNumericTest() {
+    void calculationTest() throws NoSuchMethodException {
         // given
+        var statistic = new MGSummaryProfileStatistic(new DataProfile(recordList, dsdElement), 50);
 
         // when
-
+        statistic.calculation(recordList, null);
         // then
-        fail("Not implemented");
+        Map<Object, Integer> ret = (Map<Object, Integer>) statistic.getValue();
+        Assertions.assertEquals(32, ret.size());
+        Assertions.assertEquals(4, ret.get(22));
+    }
+
+    @Test
+    void calculationNumericTest() throws NoSuchMethodException {
+        // given
+        var statistic = new MGSummaryProfileStatistic(new DataProfile(recordList, dsdElement), 50);
+
+        // when
+        statistic.calculation(recordList, null);
+        // then
+        Map<Object, Integer> ret = (Map<Object, Integer>) statistic.getValue();
+        Assertions.assertEquals(32, ret.size());
+        Assertions.assertEquals(4, ret.get(22));
+    }
+
+    @Test
+    void calculationNumericSmallTest() throws NoSuchMethodException {
+        // given
+        List<Number> data = List.of(1, 3, 3, 3, 3, 2, 1, 2, 5, 7, 8, 1, 2, 69);
+        var statistic = new MGSummaryProfileStatistic(new DataProfile(recordList, dsdElement), 5);
+
+        // when
+        statistic.calculationNumeric(data, null);
+        // then
+        Map<Object, Integer> ret = (Map<Object, Integer>) statistic.getValue();
+        Assertions.assertEquals(4, ret.size());
+        Assertions.assertEquals(3, ret.get(3));
+        Assertions.assertEquals(2, ret.get(1));
     }
 
     @Test
