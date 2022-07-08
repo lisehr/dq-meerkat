73922bbec9e3b22ecfb6268fbf011e7cd419c0ad
Rainer Meindl <rainer.meindl@scch.at>
2022-07-08 08:54:54 +0200

    Add DataSummary tests

---
+++ src/test/java/dqm/jku/dqmeerkat/quality/generator/MGSummarySkeletonGeneratorTest.java
@@ -0,0 +1,18 @@
+package dqm.jku.dqmeerkat.quality.generator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MGSummarySkeletonGeneratorTest {
+
+    @Test
+    void generateStatistics() {
+        // given
+
+        // when
+
+        // then
+        fail("not implemented");
+    }
+}
---
+++ src/test/java/dqm/jku/dqmeerkat/quality/generator/SpaceSavingSummarySkeletonGeneratorTest.java
@@ -0,0 +1,18 @@
+package dqm.jku.dqmeerkat.quality.generator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class SpaceSavingSummarySkeletonGeneratorTest {
+
+    @Test
+    void generateStatistics() {
+        // given
+
+        // when
+
+        // then
+        fail("not implemented");
+    }
+}
